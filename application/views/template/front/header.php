ed8e50a7c43ae91811aaf204f4df6c4c81a328b6
ray-watsana <ray1994watsana@gmail.com>
2017-05-29 22:53:21 +0700

    hea5

--- application/views/template/front/header.php
+++ application/views/template/front/header.php
@@ -170,39 +170,8 @@
                         </ul>
                         <!-- second-level-items -->
                     </li>
-                    
-                    <li>
-                        <a href="#"><i class="fa fa-wrench fa-fw"></i>UI Elements<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="panels-wells.html">Panels and Wells</a>
-                            </li>
-                            <li>
-                                <a href="buttons.html">Buttons</a>
-                            </li>
-                            <li>
-                                <a href="notifications.html">Notifications</a>
-                            </li>
-                            <li>
-                                <a href="typography.html">Typography</a>
-                            </li>
-                            <li>
-                                <a href="grid.html">Grid</a>
-                            </li>
-                        </ul>
-                        <!-- second-level-items -->
-                    </li>
 
-                    <li>
-                        <a href="#"><i class="fa fa-files-o fa-fw"></i>Sample Pages<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="blank.html">Blank Page</a>
-                            </li>
-                            <li>
-                                <a href="login.html">Login Page</a>
-                            </li>
-                        </ul>
+
                         <!-- second-level-items -->
                     </li>
                 </ul>
