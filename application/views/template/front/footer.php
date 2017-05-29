ef4c1dcc22e0eac7cd4678206f0522b938811299
Siriwanmw <sunare1855@gmail.com>
2017-05-29 21:17:33 +0700

    header

--- application/views/template/front/footer.php
+++ application/views/template/front/footer.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 <script src="<?php echo base_url('assets/plugins/jquery-1.10.2.js') ?>"></script>
 <script src="<?php echo base_url('assets/plugins/bootstrap/bootstrap.min.js') ?>"></script>
@@ -10,8 +10,7 @@
 <script src="<?php echo base_url('assets/plugins/morris/morris.js') ?>"></script>
 <script src="<?php echo base_url('assets/scripts/dashboard-demo.js') ?>"></script>
 
-=======
->>>>>>> a2674b956c6f42ad6465ff9d65eebd89b61c0ae8
+
       <!-- Jquery Core Js -->
     <script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js');?>"></script>
 
