205cfb871dda29de239d90dd984fb6ac2ca2f32d
Siriwanmw <sunare1855@gmail.com>
2017-05-29 16:24:45 +0700

    Initial commit

--- application/models/modelwelcome.php
+++ application/models/modelwelcome.php
@@ -6,33 +6,8 @@ class modelwelcome extends CI_Model {
 
 public function read_db()
 {
-$db = $this->db->get('member')->result_array();
+$db = $this->db->get('phonebrand')->result_array();
 return $db;
-}
-public function del($mem_id)
-{
-$this->db
-->where('mem_id',$mem_id)
-->delete('member');
-}
-public function insert($input)
-{
-$this->db->insert('member',$input);
-}
-
-public function read_one($input)
-{
-$query = $this->db
-->where('mem_id',$input)
-->get('member')
-->result_array();
-return $query;
-}
-public function update($input)
-{
-$this->db
-->where('mem_id',$input['mem_id'])
-->update('member',$input);
 }
 
 }
--- application/views/phone.php
+++ application/views/phone.php
@@ -1,1 +1,39 @@
-tttttttttttttttttttttt
+<div class="panel-body">
+  <div class="row">
+  <div class="col-lg-12">
+
+
+    <div class="row">
+      <div class="col-md-12">
+
+    <table class="table table-hover" border="0">
+      <head>
+        <th>รหัส</th>
+        <th>ชื่อโทรศัพท์</th>
+        <th>ข้อมูล่</th>
+        <th>เครือข่าย</th>
+      </head>
+    <?php foreach ($data as $key): ?>
+
+  <tbody>
+      <tr>
+      <td><?php echo $key['p_id']; ?></td>
+      <td><?php echo $key['p_name'];?></td>
+      <td><?php echo $key['p_detail'];?></td>
+      <td><?php echo $key['n_id'];?></td>
+
+
+  </tbody>
+    <?php endforeach; ?>
+  </table>
+  </div>
+  </div>
+
+</div>
+</div>
+</div>
+<!-- /.panel-body -->
+</div>
+<!--End simple table example -->
+
+</div>
