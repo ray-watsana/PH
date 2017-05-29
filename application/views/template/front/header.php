29906855eef54ba2e0f853f072ebdeea2b0e2f1e
ray-watsana <ray1994watsana@gmail.com>
2017-05-29 21:48:22 +0700

    headder2

--- application/views/template/front/header.php
+++ application/views/template/front/header.php
@@ -382,24 +382,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </nav>
         <!-- end navbar side -->
         <!--  page-wrapper -->
-      
+
 
 
 </html>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <!-- META SECTION -->
-        <title>Joli Admin - Responsive Bootstrap Admin Template</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <!-- END META SECTION -->
-
-        <!-- CSS INCLUDE -->
-        <link rel="stylesheet" type="text/css" id="theme" href="<?php echo base_url('assets/css/theme-default.css'); ?>"/>
-        <!-- EOF CSS INCLUDE -->
-    </head>
-    </html>
