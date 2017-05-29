0aaff09b019376074b667bad3660a7727faa0c6e
pattarapoljo <pattarapol93@gmail.com>
2017-05-29 17:22:00 +0700

    asset

---
+++ application/views/template/front/footer.php
@@ -0,0 +1,9 @@
+<script src="<?php echo base_url('assets/plugins/jquery-1.10.2.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/bootstrap/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/metisMenu/jquery.metisMenu.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/pace/pace.js') ?>"></script>
+<script src="<?php echo base_url('assets/scripts/siminta.js') ?>"></script>
+<!-- Page-Level Plugin Scripts-->
+<script src="<?php echo base_url('assets/plugins/morris/raphael-2.1.0.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/morris/morris.js') ?>"></script>
+<script src="<?php echo base_url('assets/scripts/dashboard-demo.js') ?>"></script>
