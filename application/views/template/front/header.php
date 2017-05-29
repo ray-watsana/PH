0833440e281b3d5fe93ec6244ab280d1b06fc06f
ray-watsana <ray1994watsana@gmail.com>
2017-05-29 23:23:23 +0700

    hear

--- application/views/template/front/header.php
+++ application/views/template/front/header.php
@@ -39,6 +39,7 @@
 
 
 
+
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-user fa-3x"></i>
@@ -74,42 +75,8 @@
                             <div class="user-section-inner">
                                 <img src="<?php echo base_url('assets/img/user.jpg') ?>" alt="">
                             </div>
-                            <div class="user-info">
-                                <div>Jonny <strong>Deen</strong></div>
-                                <div class="user-text-online">
-                                    <span class="user-circle-online btn btn-success btn-circle "></span>&nbsp;Online
-                                </div>
-                            </div>
-                        </div>
-                        <!--end user image section-->
-                    </li>
-                    <li class="sidebar-search">
-                        <!-- search section-->
-                        <div class="input-group custom-search-form">
-                            <input type="text" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                        </div>
-                        <!--end search section-->
-                    </li>
-                    <li class="selected">
-                        <a href="index.html"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="flot.html">Flot Charts</a>
-                            </li>
-                            <li>
-                                <a href="morris.html">Morris Charts</a>
-                            </li>
-                        </ul>
-                        <!-- second-level-items -->
-                    </li>
+                          
+
 
 
                         <!-- second-level-items -->
