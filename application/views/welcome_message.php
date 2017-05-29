5df84481ccd3c9b76c3ac7e64f4e3e1bb285bbaf
Siriwanmw <sunare1855@gmail.com>
2017-05-29 13:21:46 +0700

    index

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to lao </title>
+	<title>Welcome to </title>
 
 	<style type="text/css">
 
