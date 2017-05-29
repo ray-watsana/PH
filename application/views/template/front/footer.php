636289ecdebc60d7b7a7d2bb337735fdf15ab9f4
pattarapoljo <pattarapol93@gmail.com>
2017-05-29 18:12:32 +0700

    template

--- application/views/template/front/footer.php
+++ application/views/template/front/footer.php
@@ -1,9 +1,41 @@
-<script src="<?php echo base_url('assets/plugins/jquery-1.10.2.js') ?>"></script>
-<script src="<?php echo base_url('assets/plugins/bootstrap/bootstrap.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/plugins/metisMenu/jquery.metisMenu.js') ?>"></script>
-<script src="<?php echo base_url('assets/plugins/pace/pace.js') ?>"></script>
-<script src="<?php echo base_url('assets/scripts/siminta.js') ?>"></script>
-<!-- Page-Level Plugin Scripts-->
-<script src="<?php echo base_url('assets/plugins/morris/raphael-2.1.0.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/plugins/morris/morris.js') ?>"></script>
-<script src="<?php echo base_url('assets/scripts/dashboard-demo.js') ?>"></script>
+      <!-- Jquery Core Js -->
+    <script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js');?>"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.js');?>"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="<?php echo base_url('assets/plugins/bootstrap-select/js/bootstrap-select.js');?>"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="<?php echo base_url('assets/plugins/jquery-slimscroll/jquery.slimscroll.js');?>"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="<?php echo base_url('assets/plugins/node-waves/waves.js');?>"></script>
+
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="<?php echo base_url('assets/plugins/jquery-countto/jquery.countTo.js');?>"></script>
+
+    <!-- Morris Plugin Js -->
+    <script src="<?php echo base_url('assets/plugins/raphael/raphael.min.js');?>"></script>
+    <script src="<?php echo base_url('assets/plugins/morrisjs/morris.js');?>"></script>
+
+    <!-- ChartJs -->
+    <script src="<?php echo base_url('assets/plugins/chartjs/Chart.bundle.js');?>"></script>
+
+    <!-- Flot Charts Plugin Js -->
+    <script src="<?php echo base_url('assets/plugins/flot-charts/jquery.flot.js');?>"></script>
+    <script src="<?php echo base_url('assets/plugins/flot-charts/jquery.flot.resize.js');?>"></script>
+    <script src="<?php echo base_url('assets/plugins/flot-charts/jquery.flot.pie.js');?>"></script>
+    <script src="<?php echo base_url('assets/plugins/flot-charts/jquery.flot.categories.js');?>"></script>
+    <script src="<?php echo base_url('assets/plugins/flot-charts/jquery.flot.time.js');?>"></script>
+
+    <!-- Sparkline Chart Plugin Js -->
+    <script src="<?php echo base_url('assets/plugins/jquery-sparkline/jquery.sparkline.js');?>"></script>
+
+    <!-- Custom Js -->
+    <script src="<?php echo base_url('assets/js/admin.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/pages/index.js');?>"></script>
+
+    <!-- Demo Js -->
+    <script src="<?php echo base_url('assets/js/demo.js');?>"></script>
