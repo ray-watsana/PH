9b3df1c30abceac22b18f1a9a22c9824b48da682
Siriwanmw <sunare1855@gmail.com>
2017-05-30 11:08:42 +0700

    sidebar ต่อ

--- application/views/template/front/sidebar.php
+++ application/views/template/front/sidebar.php
@@ -1,35 +1,4 @@
 
-        <!-- end navbar-header -->
-        <!-- navbar-top-links -->
-
-
-
-
-
-                <!-- end dropdown-alerts -->
-            </li>
-
-            <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                    <i class="fa fa-user fa-3x"></i>
-                </a>
-                <!-- dropdown user-->
-                <ul class="dropdown-menu dropdown-user">
-                    <li><a href="#"><i class="fa fa-user fa-fw"></i>User Profile</a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-gear fa-fw"></i>Settings</a>
-                    </li>
-                    <li class="divider"></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
-                    </li>
-                </ul>
-                <!-- end dropdown-user -->
-            </li>
-            <!-- end main dropdown -->
-        </ul>
-        <!-- end navbar-top-links -->
-
-    </nav>
     <!-- end navbar top -->
 
     <!-- navbar side -->
