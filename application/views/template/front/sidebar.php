f8cc7242b7a7e21ba7cd955a9625a83500c07f23
Siriwanmw <sunare1855@gmail.com>
2017-05-30 15:14:25 +0700

    แก้sidebar

--- application/views/template/front/sidebar.php
+++ application/views/template/front/sidebar.php
@@ -166,59 +166,7 @@
                         </div>
                     </div>
                 </li>
-                <!-- END MESSAGES -->
-                <!-- TASKS -->
-                <li class="xn-icon-button pull-right">
-                    <a href="#"><span class="fa fa-tasks"></span></a>
-                    <div class="informer informer-warning">3</div>
-                    <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging">
-                        <div class="panel-heading">
-                            <h3 class="panel-title"><span class="fa fa-tasks"></span> Tasks</h3>
-                            <div class="pull-right">
-                                <span class="label label-warning">3 active</span>
-                            </div>
-                        </div>
-                        <div class="panel-body list-group scroll" style="height: 200px;">
-                            <a class="list-group-item" href="#">
-                                <strong>Phasellus augue arcu, elementum</strong>
-                                <div class="progress progress-small progress-striped active">
-                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">50%</div>
-                                </div>
-                                <small class="text-muted">John Doe, 25 Sep 2014 / 50%</small>
-                            </a>
-                            <a class="list-group-item" href="#">
-                                <strong>Aenean ac cursus</strong>
-                                <div class="progress progress-small progress-striped active">
-                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">80%</div>
-                                </div>
-                                <small class="text-muted">Dmitry Ivaniuk, 24 Sep 2014 / 80%</small>
-                            </a>
-                            <a class="list-group-item" href="#">
-                                <strong>Lorem ipsum dolor</strong>
-                                <div class="progress progress-small progress-striped active">
-                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%;">95%</div>
-                                </div>
-                                <small class="text-muted">John Doe, 23 Sep 2014 / 95%</small>
-                            </a>
-                            <a class="list-group-item" href="#">
-                                <strong>Cras suscipit ac quam at tincidunt.</strong>
-                                <div class="progress progress-small">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
-                                </div>
-                                <small class="text-muted">John Doe, 21 Sep 2014 /</small><small class="text-success"> Done</small>
-                            </a>
-                        </div>
-                        <div class="panel-footer text-center">
-                            <a href="pages-tasks.html">Show all tasks</a>
-                        </div>
-                    </div>
-                </li>
-                <!-- END TASKS -->
-            </ul>
-        </div>
-        <!-- END PAGE CONTENT -->
-    </div>
-    <!-- END PAGE CONTAINER -->
+              
 
     <!-- MESSAGE BOX-->
     <div class="message-box animated fadeIn" data-sound="alert" id="mb-signout">
@@ -238,44 +186,3 @@
             </div>
         </div>
     </div>
-    <!-- END MESSAGE BOX-->
-
-    <!-- START PRELOADS -->
-    <audio id="audio-alert" src="<?php echo base_url('assets/audio/alert.mp3');?>" preload="auto"></audio>
-    <audio id="audio-fail" src="<?php echo base_url('assets/audio/fail.mp3');?>" preload="auto"></audio>
-    <!-- END PRELOADS -->
-
-<!-- START SCRIPTS -->
-    <!-- START PLUGINS -->
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/jquery/jquery.min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/jquery/jquery-ui.min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/bootstrap/bootstrap.min.js');?>"></script>
-    <!-- END PLUGINS -->
-
-    <!-- START THIS PAGE PLUGINS-->
-    <script type='text/javascript' src='<?php echo base_url('assets/js/plugins/icheck/icheck.min.js');?>'></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/scrolltotop/scrolltopcontrol.js');?>"></script>
-
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/morris/raphael-min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/morris/morris.min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/rickshaw/d3.v3.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/rickshaw/rickshaw.min.js');?>"></script>
-    <script type='text/javascript' src='<?php echo base_url('assets/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js');?>'></script>
-    <script type='text/javascript' src='<?php echo base_url('assets/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js');?>'></script>
-    <script type='text/javascript' src='<?php echo base_url('assets/js/plugins/bootstrap/bootstrap-datepicker.js');?>'></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/owl/owl.carousel.min.js');?>"></script>
-
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/moment.min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins/daterangepicker/daterangepicker.js');?>"></script>
-    <!-- END THIS PAGE PLUGINS-->
-
-    <!-- START TEMPLATE -->
-    <script type="text/javascript" src="<?php echo base_url('assets/js/settings.js');?>"></script>
-<!-->settings load<-->
-    <script type="text/javascript" src="<?php echo base_url('assets/js/plugins.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/actions.js');?>"></script>
-
-    <script type="text/javascript" src="<?php echo base_url('assets/js/demo_dashboard.js');?>"></script>
-    <!-- END TEMPLATE -->
-<!-- END SCRIPTS -->
